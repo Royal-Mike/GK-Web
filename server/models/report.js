48083bffe728af39a0086990c7142fb2d57d6888
ThanhKy <61177006+thanh634@users.noreply.github.com>
2024-06-10 02:28:10 +0700

    update test-run.hbs ( temp ) + add all models

---
+++ server/models/report.js
@@ -0,0 +1,25 @@
+﻿// models/report.js
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Report = sequelize.define('Report', {
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        created_by_user_id: DataTypes.INTEGER,
+        related_test_case_id: DataTypes.INTEGER,
+        related_test_run_id: DataTypes.INTEGER,
+        related_project_id: DataTypes.INTEGER,
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        timestamps: false
+    });
+    Report.associate = function (models) {
+        Report.belongsTo(models.User, { foreignKey: 'created_by_user_id' });
+        Report.belongsTo(models.TestCase, { foreignKey: 'related_test_case_id' });
+        Report.belongsTo(models.TestRun, { foreignKey: 'related_test_run_id' });
+        Report.belongsTo(models.Project, { foreignKey: 'related_project_id' });
+    };
+    return Report;
+};

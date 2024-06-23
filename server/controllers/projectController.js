1ef3c3ced1b95cbec0d7cbddc0e53c97ddf2ae43
HSQMG <9981hoang@gmail.com>
2024-06-23 15:55:55 +0700

    function sort issues

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -366,6 +366,56 @@ controller.issuesView = async (req, res, next) => {
       },
     });
 
+
+    // sort issues
+  //   const projectId = req.params.id;
+  //   const sortType = req.query.sortType || "CreatedAt"; 
+  //   const sortOrder = req.query.sortOrder || "Asc"; 
+  //   console.log(sortType, sortOrder)
+  //   let issues = await models.Issue.findAll({
+  //     where: {
+  //       project_id: projectId,
+  //     },
+  //     include: [
+  //       {
+  //         model: models.Member,
+  //         as: "Creator",
+  //         include: [
+  //           {
+  //             model: models.User,
+  //             attributes: ["first_name", "last_name"],
+  //             required: false,
+  //           },
+  //         ],
+  //         required: false,
+  //       },
+  //     ],
+  //   });
+    
+  //   issues = issues.sort((a, b) => {
+  //     switch (sortType) {
+  //       case "Priority":
+  //         return sortOrder === "Asc"
+  //           ? a.priority.localeCompare(b.priority)
+  //           : b.priority.localeCompare(a.priority);
+  //       case "Code":
+  //         return sortOrder === "Asc" ? a.id - b.id : b.id - a.id;
+  //       case "CreatedAt":         
+  //         return sortOrder === "Asc"
+  //           ? new Date(a.createdAt) - new Date(b.createdAt)
+  //           : new Date(b.createdAt) - new Date(a.createdAt);
+  //       default:
+  //         return 0;
+  //       }
+  //     })
+  //   res.render("issue", {
+  //     layout: "main_layout",
+  //     issues: issues,
+  //   });
+  // } catch (error) {
+  //   console.error("Error fetching issues:", error);
+  //   res.status(500).send("An error occurred while fetching issues.");
+  // }
     // Render issues view and pass data
     res.render("developer/issues", { projectId, issues });
   } catch (error) {
@@ -413,6 +463,7 @@ controller.issueDetailView = async (req, res, next) => {
     next(error);
   }
 };
+
 
 controller.reportView = async (req, res, next) => {
   try {
